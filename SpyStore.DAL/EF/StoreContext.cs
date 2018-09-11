b4aa3f3f2f837bf3baae2d774b54a18d125601cf
Tyrell Frame <tlframe2@gmail.com>
2018-09-11 00:13:56 -0400

    initial commit

---
+++ SpyStore.DAL/EF/StoreContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using SpyStore.Models.Entities;
+
+namespace SpyStore.DAL.EF
+{
+    public class StoreContext : DbContext 
+    {
+        public StoreContext()
+        {
+
+        }
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SpyStore;Trusted_Connection=True;
+                                              MultipleActiveResultSets=true;", options => options.EnableRetryOnFailure());
+            }
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ SpyStore.Models/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using SpyStore.Models.Entities.Base;
+
+namespace SpyStore.Models.Entities
+{
+    [Table("Categories", Schema = "Store")]
+    public class Category : EntityBase
+    {
+        [DataType(DataType.Text), MaxLength(50)]
+        public string CategoryName { get; set; }
+    }
+}
